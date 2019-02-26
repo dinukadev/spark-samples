48820ba2614b123a740c68e6627d510ba51b3713
Dinuka Arseculeratne <dinukaarseculeratne@Dinukas-MacBook-Pro.local>
2019-02-27 10:36:57 +1100

    change to the word count to include regular expressions

--- src/main/scala/com/sundogsoftware/spark/WordCount.scala
+++ src/main/scala/com/sundogsoftware/spark/WordCount.scala
@@ -12,9 +12,11 @@ object WordCount {
 
     val input = sc.textFile("src/main/resources/book.txt")
 
-    val words = input.flatMap(x=> x.split(" "))
+    val words = input.flatMap(x=> x.split("\\W+"))
 
-    val wordCount = words.countByValue()
+    val lowerCaseWords = words.map(x=> x.toLowerCase())
+
+    val wordCount = lowerCaseWords.countByValue()
 
     wordCount.foreach(println)
   }
